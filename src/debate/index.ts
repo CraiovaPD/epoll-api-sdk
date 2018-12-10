9df37a8bc4207d7c4c1c0d3aa495161e2a829056
Dragos Sebestin <sebestin.dragos@gmail.com>
2018-12-10 03:08:50 +0200

    [master] Added implementation for debate API methods.

--- src/debate/index.ts
+++ src/debate/index.ts
@@ -55,4 +55,71 @@ export class Debate {
     });
   }
 
+  /**
+   * Get debate by id.
+   */
+  getDebateById (id: string) : Observable<IDebate<any>> {
+    return this._http.get(`${this._settings.apiBaseUrl}/debate/${id}`, {});
+  }
+
+  /**
+   * Add a new poll option.
+   */
+  addPollOption (params: {
+    pollId: string,
+    reason: string
+  }) : Observable<any> {
+    return this._http.post(`${this._settings.apiBaseUrl}/debate/poll/${params.pollId}/option`, {
+      body: {
+        reason: params.reason
+      }
+    });
+  }
+
+  /**
+   * Remove a poll option.
+   */
+  removePollOption (params: {
+    pollId: string,
+    optionId: string
+  }) : Observable<any> {
+    return this._http.delete(`${this._settings.apiBaseUrl}/debate/poll/${params.pollId}/option/${params.optionId}`, {});
+  }
+
+  /**
+   * Add a new poll attachment.
+   */
+  addPollAttachment (params: {
+    pollId: string,
+    formData: any
+  }) : Observable<any> {
+    return this._http.post(`${this._settings.apiBaseUrl}/debate/poll/${params.pollId}/attachment`, {
+      body: params.formData
+    });
+  }
+
+  /**
+   * Remove a poll attachment.
+   */
+  removePollAttachment (params: {
+    pollId: string,
+    attachmentId: string
+  }) : Observable<any> {
+    return this._http.delete(`${this._settings.apiBaseUrl}/debate/poll/${params.pollId}/attachment/${params.attachmentId}`, {});
+  }
+
+  /**
+   * Add a new poll vote.
+   */
+  addPollVote (params: {
+    pollId: string,
+    optionId: string
+  }) : Observable<any> {
+    return this._http.post(`${this._settings.apiBaseUrl}/debate/poll/${params.pollId}/vote`, {
+      body: {
+        optionId: params.optionId
+      }
+    });
+  }
+
 }
